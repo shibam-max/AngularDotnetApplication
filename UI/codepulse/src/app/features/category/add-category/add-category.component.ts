ff8e879f99a3cba22cf0533d7687588b71dbd730
shibam-max <shibamsamaddar1999@gmail.com>
2025-08-29 22:54:35 +0530

    Added formSubmit feature to send form data from angular component to APIs

--- UI/codepulse/src/app/features/category/add-category/add-category.component.ts
+++ UI/codepulse/src/app/features/category/add-category/add-category.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { AddCategoryRequest } from '../models/add-category-request.model';
 
 @Component({
   selector: 'app-add-category',
@@ -9,5 +10,22 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './add-category.component.css'
 })
 export class AddCategoryComponent {
+
+
+  model: AddCategoryRequest;
+
+  constructor() {
+    this.model = {
+      name: '',
+      urlHandle: ''
+    };
+  }
+
+  onFormSubmit() {
+    console.log(this.model);
+
+
+
+  }
 
 }
