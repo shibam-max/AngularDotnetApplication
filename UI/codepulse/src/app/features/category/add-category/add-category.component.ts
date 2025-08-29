3e347401792b0ad53a3b465b1b02df3395ef1ef0
shibam-max <shibamsamaddar1999@gmail.com>
2025-08-29 21:46:03 +0530

    Add Angular 18 frontend files

---
+++ UI/codepulse/src/app/features/category/add-category/add-category.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-add-category',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './add-category.component.html',
+  styleUrl: './add-category.component.css'
+})
+export class AddCategoryComponent {
+
+}
---
+++ UI/codepulse/src/app/features/category/category-list/category-list.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-category-list',
+  standalone: true,
+  imports: [RouterLink],
+  templateUrl: './category-list.component.html',
+  styleUrl: './category-list.component.css'
+})
+export class CategoryListComponent {
+
+}
