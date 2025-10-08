c710816a615de8599c0b3e9acdc49c68a4920f87
shibam-max <shibamsamaddar1999@gmail.com>
2025-10-08 23:04:04 +0530

    Add backend API endpoints: GetById and Update category operations with repository pattern implementation

--- API/CodePulse.API/CodePulse.API/Controllers/CategoriesController.cs
+++ API/CodePulse.API/CodePulse.API/Controllers/CategoriesController.cs
@@ -42,5 +42,50 @@ namespace CodePulse.API.Controllers
 
             return Ok(response);
         }
+
+        // GET: https://localhost:7097/api/Categories
+        [HttpGet]
+        public async Task<IActionResult> GetAllCategories()
+        {
+            var categories = await categoryRepository.GetAllAsync();
+
+            // Map Domain model to DTO
+
+            var response = new List<CategoryDto>();
+            foreach (var category in categories)
+            {
+                response.Add(new CategoryDto
+                {
+                    Id = category.Id,
+                    Name = category.Name,
+                    UrlHandle= category.UrlHandle
+                });
+            }
+
+            return Ok(response);
+        }
+
+        // GET: https://localhost:7097/api/Categories/{id}
+        [HttpGet]
+        [Route("{id:Guid}")]
+        public async Task<IActionResult> GetCategoryById([FromRoute] Guid id)
+        {
+            var existingCategory = await categoryRepository.GetById(id);
+
+            if (existingCategory is null)
+            {
+                return NotFound();
+            }
+
+            var response = new CategoryDto
+            {
+                Id = existingCategory.Id,
+                Name = existingCategory.Name,
+                UrlHandle = existingCategory.UrlHandle
+            };
+
+            return Ok(response);
+        }
+
     }
 }
--- API/CodePulse.API/CodePulse.API/Repositories/Interface/ICategoryRepository.cs
+++ API/CodePulse.API/CodePulse.API/Repositories/Interface/ICategoryRepository.cs
@@ -5,5 +5,9 @@ namespace CodePulse.API.Repositories.Interface
     public interface ICategoryRepository
     {
         Task<Category> CreateAsync(Category category);
+
+        Task<IEnumerable<Category>> GetAllAsync();
+
+        Task<Category?> GetById(Guid id);
     }
 }
